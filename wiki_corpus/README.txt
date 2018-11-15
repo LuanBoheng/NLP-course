c691f6a1432b5e737278554ba51b82ce1286f0eb
LuanBoheng <314887027@qq.com>
2018-11-15 23:32:53 +1100

    繁简转换

--- wiki_corpus/README.txt
+++ wiki_corpus/README.txt
@@ -8,6 +8,9 @@ git clone https://github.com/attardi/wikiextractor
 extract xml files to folder ‘wiki_ch’:
 bash extract_wikich.sh
 
-繁简转换：
+繁简转换(single file):
 install: sudo apt-get install opencc
 command: opencc -i input_file -o output_file -c t2s.json
+
+or run following script convert all corpus:
+bash traditional_to_simplified.sh
--- wiki_corpus/traditional_to_simplified.sh
+++ wiki_corpus/traditional_to_simplified.sh
@@ -4,9 +4,12 @@ echo $path
 
 for folder in $(ls wiki_ch) 
 do 
-	echo "folder: $folder"
-	for file in $(ls "$path/wiki_ch/$folder")
-	do
-		echo "    file: $file"
+    echo "folder: $folder"
+    mkdir wiki_chs/$folder
+    for file in $(ls "$path/wiki_ch/$folder")
+    do
+        input_file="wiki_ch/$folder/$file"
+        output_file="wiki_chs/$folder/$file"
+        opencc -i $input_file -o $output_file -c t2s.json
 	done
 done
